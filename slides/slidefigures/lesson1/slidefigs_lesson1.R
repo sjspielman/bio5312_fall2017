7cd4f700b748eefc3940fd061d686c45954a210f
Stephanie Spielman <stephanie.spielman@gmail.com>
2017-08-29 11:52:41 -0400

    Added violin and density to day1

--- slides/slidefigures/lesson1/slidefigs_lesson1.R
+++ slides/slidefigures/lesson1/slidefigs_lesson1.R
@@ -2,11 +2,20 @@
 
 require(dplyr)
 require(cowplot)
+require(tidyr)
 
 ### Histogram
 dat <- data.frame(x = rnorm(500, mean=15))
 ggplot(dat, aes(x =x)) + geom_histogram(fill = "dodgerblue", color = "black") + xlab("Value") + ylab("Count") -> histo
 ggsave("example_histogram.pdf", histo)
+
+### Density
+ggplot(dat, aes(x=x)) + geom_density(fill = "chartreuse4", color = "black") + xlab("Value") + ylab("Density") -> denso
+ggsave("example_density.pdf", denso)
+ggplot(dat, aes(x=x)) + geom_density(fill = "chartreuse4", color = "black", alpha=0.4)  -> denso2
+ggplot(dat, aes(x=x)) + geom_histogram(fill = "white", color = "black")  -> histo2
+ggsave("example_density_overlay.pdf", denso2)
+ggsave("example_histogram_overlay.pdf", histo2)
 
 
 ### Boxplots
@@ -19,6 +28,17 @@ ggplot(boxes,  aes(x = x, y = y, fill=x)) + geom_boxplot() + scale_fill_manual(v
 ggsave("boxplots_misleading.pdf", misleadingboxes)
 ggplot(boxes2, aes(x=y, fill=x2)) + geom_histogram(color="black") + facet_grid(~x2) + scale_fill_manual(values=c("forestgreen", "coral3")) + xlab("Value") + ylab("Count") + theme(legend.position = "none") -> histosss
 ggsave("histograms_uni_bi.pdf", histosss)
+
+
+### Violin
+dat <- data.frame(a = rnorm(1000, mean=5, sd = 2), b= rnorm(1000, mean=2, sd = 1), c=rnorm(1000, mean=4, sd = 0.3))
+dat %>% gather(key, value, a:c) -> dat2
+#names(dat2) <- c("key", "value", "N(5,4)", "N(2,1)", "N(4,0.09)")
+ggplot(dat2, aes(x="",y=value, fill=key)) + geom_violin(color = "black") + scale_fill_manual(values=c("coral1", "coral3", "coral")) + facet_grid(~key) + theme(strip.background = element_blank(), strip.text.x = element_blank(), legend.position="none") -> v3
+ggplot(dat2, aes(x=value,fill=key)) + geom_density(color = "black") + scale_fill_manual(values=c("coral1", "coral3", "coral")) + facet_wrap(~key, scales="free") + theme(strip.background = element_blank(), strip.text.x = element_blank(), legend.position="none")  -> d3
+ggplot(dat2, aes(x="", y=value, fill=key)) + geom_boxplot(color = "black", size=0.5, outlier.size=0.5) + scale_fill_manual(values=c("coral1", "coral3", "coral")) + facet_grid(~key) + theme(strip.background = element_blank(), strip.text.x = element_blank(), legend.position="none")  -> b3
+together <- plot_grid(v3, d3, b3, nrow=3)
+save_plot("violin_density_box.pdf", together, base_height=8)
 
 ### Barplot
 dat2 <- data.frame(flowers = c(rep("red", runif(1, 10, 70)), 
